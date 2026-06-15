e1d4b5547256851c40089698624b3ca8990e543b
Joselu <joseluiscc103@gmail.com>
2026-06-15 16:11:45 +0200

    Create UserRepository class

---
+++ src/users/repository.py
@@ -0,0 +1,17 @@
+# src/users/respository.py
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.shared.repository import BaseRepository
+
+from src.users.models import User
+
+
+class UserRepository(BaseRepository[User]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(session, User)
+
+    async def get_by_email(self, email: str) -> User | None:
+        """Return user registered with the indicated email"""
+        stmt = select(User).where(User.email == email)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
