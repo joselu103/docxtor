29768ffe0e20bc922b5a82cbe1bf1e0c916e39fb
Joselu <joseluiscc103@gmail.com>
2026-06-17 14:12:37 +0200

    Fix broken relationship on User model

--- src/users/models.py
+++ src/users/models.py
@@ -1,13 +1,9 @@
 # src/users/models.py
-from typing import TYPE_CHECKING
-
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from src.chats.models import Chat
 from src.shared.models import BaseModel
-
-if TYPE_CHECKING:
-    from src.chats.models import Chat
 
 
 class User(BaseModel):
@@ -17,4 +13,4 @@ class User(BaseModel):
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
 
-    chats: Mapped[list["Chat"]] = relationship("Chat")
+    chats: Mapped[list[Chat]] = relationship("Chat")
